26a3af204b38fa576d22da74e3eb5640f5dd8f3c
Rofy-Ray <okyereforsonr@berea.edu>
2023-02-17 17:24:56 -0600

    Fix in urls file

--- appwei/urls.py
+++ appwei/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('', views.index, name='index'),
 ]
 
-if wei.settings.development.DEBUG:
+if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

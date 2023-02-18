d53ab927d21c01dc18b1645b015bac8576a6a74d
Rofy-Ray <okyereforsonr@berea.edu>
2023-02-17 21:22:54 -0600

    Getting static files to load

--- wei/urls.py
+++ wei/urls.py
@@ -18,8 +18,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+
 
 urlpatterns = [
     path('', include('appwei.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
